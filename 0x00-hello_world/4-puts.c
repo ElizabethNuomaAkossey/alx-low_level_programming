4c852599f011900ddfcf5550846751bfe16e21b5
ElizabethNuomaAkossey <elizabethnuomaakossey@gmailcom>
2023-02-16 21:15:48 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+\**
+*main-start point
+*
+*Return: returned 0 value(correct)
+*/
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
