c3954ab5e2d34894ea0a1c57896606912b7e3200
ElizabethNuomaAkossey <elizabethnuomaakossey@gmailcom>
2023-02-19 10:41:16 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+/**
+ * main - Prints all single digit numbers in base 10
+ * starting from 0, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 48; n < 57; n++)
+	{
+		putchar (n);
+	}
+	putchar ('\n');
+	return (0);
+}
