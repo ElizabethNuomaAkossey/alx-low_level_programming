07f981a0040ff4963d3485af36312ebb8fad9593
ElizabethNuomaAkossey <elizabethnuomaakossey@gmailcom>
2023-02-22 19:19:40 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,6 +27,12 @@ void print_times_table(int n)
 					_putchar(32);
 					_putchar(m + 48);
 				}
+				else if (m <= 99)
+				{
+					_putchar(32);
+					_putchar((m / 10) + 48)
+					_putchar((m % 10) + 48);
+				}
 				else
 				{
 					_putchar(((m / 100) % 10) +  48);
