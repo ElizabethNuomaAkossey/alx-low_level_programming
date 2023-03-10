3505e3efbb93e400d725d603e0ba788299414e7f
ElizabethNuomaAkossey <elizabethnuomaakossey@gmailcom>
2023-03-09 18:28:30 -0800

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to find length
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	strlent = 0;
+
+	if (*s)
+	{
+		strlent++;
+		strlent += _strlen_recursion(s + 1);
+	}
+	return (strlent);
+}
